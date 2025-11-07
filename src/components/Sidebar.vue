51e6684f3b61cbf11a8eeb6d2fbde946ce98bca7
Ahmed Tarek  Al-Ashry <45108992+AhmedEko@users.noreply.github.com>
2025-11-08 01:19:13 +0200

    Delete src directory

--- src/components/Sidebar.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <nav>
-    <router-link to="/">Home</router-link>
-    <router-link to="/study-materials">Study Materials</router-link>
-  </nav>
-</template>
--- src/main.js
+++
@@ -1,7 +0,0 @@
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import '@fortawesome/fontawesome-free/css/all.css'
-
-createApp(App).use(router).mount('#app')
